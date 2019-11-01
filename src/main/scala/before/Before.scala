954b2c59bb4fbbd30afc9a470cb786dac6c1c2d3
Gabor Bakos <bakos.gabor@mind-era.com>
2019-11-01 20:39:12 +0100

    Less ugly mutable implementation

--- src/main/scala/before/Before.scala
+++ src/main/scala/before/Before.scala
@@ -5,25 +5,25 @@ import scala.jdk.CollectionConverters._
 
 object Before extends App {
   def change(nums: mutable.ArrayBuffer[Int], references: Seq[Int], transform: PartialFunction[(Int, Int), Seq[Int]]): Unit = {
-    var newValues = mutable.Seq[Int]()
+    val newValues = mutable.ArrayBuffer[Int]()
     for {
       num <- nums
       ref <- references
     } {
       newValues ++= transform.lift((num, ref)).getOrElse(Seq.empty)
     }
-    nums.asJava.clear()
-    nums.asJava.addAll(newValues.asJava)
+    nums.clear()
+    nums ++= newValues
   }
 
   val input = mutable.ArrayBuffer(0, 1, 42)
   val reference = Seq(2, 3, 5, 7, 11)
   val divisors: PartialFunction[(Int, Int), Seq[Int]] = {
     case (nonPos, _) if nonPos <= 0 => Seq.empty[Int]
-    case (a, b) => var powers = mutable.Seq[Int]()
+    case (a, b) => val powers = mutable.ArrayBuffer[Int]()
       var newB = b
       while (a % newB == 0) {
-        powers :+= newB
+        powers += newB
         newB *= b
       }
       powers.toSeq
