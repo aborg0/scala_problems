f45886d6c5fb7d589b5890f8b0863cc38720cd07
Gabor Bakos <bakos.gabor@mind-era.com>
2020-03-28 19:27:18 +0100

    Remove unrelated code

--- src/main/scala/MapValues.scala
+++ src/main/scala/MapValues.scala
@@ -34,31 +34,4 @@ object MapValues extends App {
 
   println(s"thing(0): ${newDistributionMap.get(thing(0))}, thing(1): ${newDistributionMap.get(thing(1))}")
   println(s"thing(0): ${newDistributionMap.get(thing(0))}, thing(1): ${newDistributionMap.get(thing(1))}")
-
-  //  val cache: scala.collection.mutable.Map[Int, Int] = scala.collection.mutable.Map[Int, Int]()
-  //
-  //  def fibNaive(n: Int): Int = {
-  //    require(n > 0)
-  //    if (n < 3) 1 else fibNaive(n - 1) + fibNaive(n - 2)
-  //  }
-  //  def fib(n: Int): Int = {
-  //    require(n > 0)
-  //    if (n < 3) 1 else cache.getOrElseUpdate(n - 2, fib(n - 2)) + cache.getOrElseUpdate(n - 1, fib(n - 1))
-  //  }
-  //
-  //  def time[T](block: => T): T = {
-  //    val start = System.nanoTime()
-  //    val res = block
-  //    val end = System.nanoTime()
-  //    println(s"Took: ${(end - start) * 1E-6} ms, got: $res")
-  //    res
-  //  }
-  //
-  ////  time(fibNaive(44))
-  //  time(fib(44))
-  //  val almostMap = time(cache.mapValues(f => math.log(f) / math.log(.5+math.sqrt(5) / 2)))
-  //  time(almostMap(33))
-  //  time(almostMap(43))
-  //  time(cache(33))
-  //  time(cache(43))
 }
