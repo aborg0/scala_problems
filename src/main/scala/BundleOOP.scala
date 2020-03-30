b66037a6b27353107d017adcc965888a283b36a8
Gabor Bakos <bakos.gabor@mind-era.com>
2020-03-30 19:34:37 +0200

    final or abstract (motivating) example

---
+++ src/main/scala/BundleOOP.scala
@@ -0,0 +1,13 @@
+trait BundleOOP {
+  def initialize(): Unit = {}
+}
+
+class Activator(token: String)
+
+class BundleOOPImpl(token: String) extends BundleOOP {
+  private[this] var activator = _
+  override def initialize(): Unit = {
+    super.initialize()
+    this.activator = new Activator(token)
+  }
+}
