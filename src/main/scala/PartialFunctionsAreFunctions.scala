ff2fcaf35a3e3d05c63a67aa709179c204f4b4b2
Gabor Bakos <bakos.gabor@mind-era.com>
2020-04-20 21:05:18 +0200

    Fix for more strict options

--- src/main/scala/PartialFunctionsAreFunctions.scala
+++ src/main/scala/PartialFunctionsAreFunctions.scala
@@ -4,7 +4,7 @@ object PartialFunctionsAreFunctions {
   def main(args: Array[String]): Unit = {
     Try(???).recover(_ => 1)
 
-    List("FOO_A", "BAR_B").map {
+    val _ = List("FOO_A", "BAR_B").map {
       case s if s.startsWith("FOO_") => s
     }
   }
